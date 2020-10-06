55c7b8f0178ebad04dd9a8e6bf2f32dbc1c2b6e2
ArindamMohanty <69613665+arindammohanty@users.noreply.github.com>
2020-10-06 19:06:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,155 +1,142 @@
-
-var obstacle, obstacleImage, obstacleGroup;
+//calling variables
+  var obstacle, obstacleImage, obstacleGroup;
   var banana, bananaImage, bananaGroup;
   var monkey, monkey_running;
-  var backgroundy, backgroundImage;
+  var background1, background2, backgroundImage;
   var score= 0;
   var ground;
-  var gamestate = 1;
-  var reset,reset1;
 
 function preload(){
-   
+  //loading image for background  
     backgroundImage= loadImage("jungle.jpg");
-
+  
+  //loading animation for monkey
   monkey_running= loadAnimation ("Monkey_01.png", "Monkey_02.png", "Monkey_03.png", "Monkey_04.png", "Monkey_05.png", "Monkey_06.png", "Monkey_07.png", "Monkey_08.png", "Monkey_09.png", "Monkey_10.png");
   
+  //loading images for banana and obstacles
     bananaImage= loadImage("banana.png");
     obstacleImage= loadImage("stone.png");
-  
-    reset1 = loadImage("RESET.png")
 }
 
 function setup() {
- 
-    createCanvas(400,400);
+  //creating canvas  
+    createCanvas(windowWidth, windowHeight);
   
-    backgroundy = createSprite (200,200,10,10);
-    backgroundy.addImage ("backgroundimage", backgroundImage);
-    backgroundy.velocityX= -2;
+  //creating background sprite
+    background1= createSprite (windowWidth/2,windowHeight/2);
+    background1.addImage ("backgroundimage", backgroundImage);
+    background1.velocityX= -2;
+    background2= createSprite (windowWidth*2,windowHeight/2);
+    background2.addImage ("backgroundimage", backgroundImage);
+    background2.velocityX= -2;
     
-    monkey = createSprite(50,340,10,10);
+  //creating monkey sprite
+    monkey= createSprite (windowWidth/8,windowHeight/2+300,10,10);
     monkey.addAnimation ("monkeyrunning", monkey_running);
-    monkey.scale= 0.1;
   
-    reset = createSprite(200,200,10,10);
-    reset.addImage("reset3",reset1);
-    reset.scale = 0.5;
-
-    ground= createSprite (0,390,800,10);
+  //creating ground sprite
+    ground= createSprite (windowWidth/8,windowHeight/2+300,800,10);
     ground.visible= false;
   
+  //creating groups for banana and obstacles
     bananaGroup= new Group ();
     obstacleGroup= new Group ();
 }
 
 function draw() {
+  //assigning background color
     background("white");
+  
+  //to know the position of monkey to make more changes
     console.log(monkey.x);
   
-    if (backgroundy.x<150) {
-      backgroundy.x= 200
-    }   
-   
-    if(gamestate == 1){
-      reset.visible = false;
+  //reseting background
+    if (background1.x<windowWidth/8) {
+      background1.x= windowWidth/2;
+    } 
+    if (background2.x<windowWidth/2) {
+      background2.x= windowWidth;
     }
   
-    if(gamestate == 0){
-      fill("red");
-    text("You loose.",300,100);
-    reset.visible = true;
-      if(mousePressedOver(reset)) {
-      reset2();
-      }
-    }
-  
-  
-    if (keyDown ("space") && monkey.x>=20) {
+  //making the monkey jump  
+    if (touches.length > 0 ||keyDown ("space") && monkey.x>=50) {
       monkey.velocityY= -20;  
+      touches = [];
     }    
+    
+  //adding gravity to monkey
     monkey.velocityY= monkey.velocityY + 0.8;
+  
+  //preventing the monkey from falling off the ground
     monkey.collide (ground);
   
+  //scoring system and changing size of the monkey
     if (bananaGroup.isTouching(monkey)) {
       score= score+2;
       bananaGroup.destroyEach();
     }
   
-    if(score == 50){
-      gamestate = 2;
-      text.scale = 20;
-    }
-  
     switch (score) {
-      case 0:monkey.scale = 0.1;
+      case 0: monkey.scale= 0.15;
       break;
-      case 10: monkey.scale= 0.15;
+      case 10: monkey.scale= 0.20;
       break;
-      case 20: monkey.scale= 0.20;
+      case 20: monkey.scale= 0.25;
       break;
-      case 30: monkey.scale= 0.25;
+      case 30: monkey.scale= 0.30;
       break;
-      case 40: monkey.scale= 0.30;
+      case 40: monkey.scale= 0.35;
       break;
-      case 50: monkey.scale= 0.35;
+      case 50: monkey.scale= 0.40;
       break;
       default: break;
     }
   
-    if (obstacleGroup.isTouching(monkey) && gamestate == 1 ) {
+    if (obstacleGroup.isTouching(monkey)) {
       score= 0;
       obstacleGroup.destroyEach();
       monkey.scale= 0.1;
-      gamestate = 0;
     }
+  
+   
+  //calling user-defined functions
     spawnBananas();
     spawnObstacles();
   
+  //drawing sprites
     drawSprites();
+  
+  //displaying score
     stroke ("white");
     textSize (15);
-    text ("Score: "+score,190,70);
-  
-    if(gamestate ==2){
-    fill("green");
-    textSize(20);
-      text("you win",160,170);
-    
-    reset.visible = true;
-    if(mousePressedOver(reset)) {
-    reset2();
-    }
- }
-  
+    text ("Score: "+score,windowWidth/2-30,70);  
 }
 
 //function for bananas
 function spawnBananas () {
-  if (frameCount%90===0 && gamestate == 1) {
-    banana= createSprite (360,120,10,10);  
+  if (frameCount%180===0) {
+    banana= createSprite (windowWidth,windowHeight/2,10,10);  
     banana.addImage ("bananaimage", bananaImage);
-    banana.scale= 0.06;
+    banana.scale= 0.10;
     banana.velocityX= -3;
-      banana.lifetime= 150;
+    
+    //adding lifetime to bananas
+      banana.lifetime= windowWidth;
+    
+    //adding banana to banana group
       bananaGroup.add(banana);
   }
 }
 
 //function for obstacles
 function spawnObstacles () {
-  if (frameCount%300===0 && gamestate == 1) {
-    obstacle= createSprite (270,370,10,10);
+  if (frameCount%300===0) {
+    obstacle= createSprite(windowWidth,windowHeight/2+300,10,10);
     obstacle.addImage ("obstacleimage", obstacleImage);
-    obstacle.scale= 0.05;
+    obstacle.scale= 0.15;
     obstacle.velocityX= -4;
+    
+    //adding obstacle to obstacle group
       obstacleGroup.add(obstacle);
   }
-}
-function reset2(){
-  gamestate = 1;
-  obstacleGroup.destroyEach();
-  bananaGroup.destroyEach();
-  score =0;
-
 }
